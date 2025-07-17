ca8f00db00893e16446e13bbac6f63b9e24d6347
nmxr5 <manaralharby7@gmail.com>
2025-07-17 23:24:17 +0300

    first deploy

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,17 @@
+from flask import Flask, request
+import telebot
 import os
-from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(_name_)
+TOKEN = "8090368961:AAHLbisTtk844DgZm1qv-finteOELWeaSF4"
+bot = telebot.TeleBot(TOKEN)
 
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    port = int(os.environ.get("PORT", 10000))  # هنا التغيير
-    app.run(host='0.0.0.0', port=port)
+@app.route('/', methods=['POST'])
+def receive_update():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '', 200
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    app.run(host='0.0.0.0', port=10000)
